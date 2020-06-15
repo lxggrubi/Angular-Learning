09087c7d04f5643fe76e35d1866f1f74ea90428f
leongrubisic <lxg6892@gmail.com>
2020-06-15 15:30:43 +0200

    Updates

---
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Lasagna", "Garfield's favourite food", "http://tiny.cc/opquqz"),
+    new Recipe("Bolognese", "Garfield's favourite food", "http://tiny.cc/opquqz")
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
